4be6ea22b88968d1c0ab1cd09c73da2f6af76cad
Arc-Data <arclight04@gmail.com>
2023-02-15 13:53:03 +0800

    created training form modal

--- index.php
+++ index.php
@@ -11,8 +11,20 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Training Management System</title>
+	<script type="text/javascript" src = "assets/js/training-form.js" defer></script>
 </head>
 <body>
-
+	<button id = 'create-training-button'>Create Training</button>
+	<dialog id = 'training-form'>
+		<div>
+			<form method = "POST" action = 'index.php'>
+				<br>
+				<br>
+				<br>
+				<br>
+				<button type = "submit">Submit</button>
+			</form>
+		</div>
+	</dialog>
 </body>
 </html>
