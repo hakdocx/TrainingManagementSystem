6276448dc31ee13ccdc433918961fa365554811c
Arc-Data <arclight04@gmail.com>
2023-02-19 12:49:15 +0800

    added header file

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 
 	require 'templates/connection.php';
+	require 'templates/header.php';
 
 	session_start();
 
@@ -43,26 +44,6 @@
 
 ?>
 
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Training Management System</title>
-  <!-- Google Fonts -->
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-
-  <!-- CSS File -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-  <!-- JS File -->
-	<script type="text/javascript" src = "assets/js/training-form.js" defer></script>
-</head>
 <body>
 	<dialog id = 'training-form'>
 		<form method = "POST" action = "code.php">
@@ -165,7 +146,7 @@
 							<?php if($page > 1) { ?>
 								<li class="page-item">
 									<a class="page-link" href = "index.php?page=<?= $page - 1 . retainSearch() ?>">
-										Previous
+										<i class="fa-solid fa-angle-left"></i>
 									</a>
 								</li>
 							<?php } ?>
@@ -205,7 +186,7 @@
 							<?php if($page < $totalPages) { ?>
 								<li class="page-item">
 									<a class="page-link" href = "index.php?page=<?= $page + 1 . retainSearch() ?>">
-										Next
+										<i class="fa-solid fa-angle-left"></i>
 									</a>
 								</li>
 							<?php } ?>
