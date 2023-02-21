16abf8a3c291822107edbca4854b07ce776ae547
luwesaram <luwesaram@gmail.com>
2023-02-21 16:02:54 +0800

    fixed outputs of view record, fixed html css

--- viewRecord.php
+++ viewRecord.php
@@ -17,45 +17,62 @@
 <dialog id = 'deleteForm'>
   Delete This ?
 </dialog>
-<body style = "font-family: Montserrat; overflow-x:hidden; background-color:#fffcfa;">
+<body style = "font-family: Montserrat; overflow-x:hidden; background-color:#fefcfb;">
   <div class = "box">
     <div class="row pb-3" style="background-color: #681a1a; color: white; padding-left:100px; padding-top: 60px;">
+        <a href="index.php" style="text-decoration:none; color:white;">
+          &#8592; Back to View
+        </a>
         <h1 style = "font-size: 48px;">
          <?php echo $result['course_title']; ?>
         </h1>
-        <h6>
+        <h5>
           #
         <?php echo $result ['number_of_days'] . " Days"; ?>
-        </h6>
+        </h5>
     </div>      
-    <div class="container mt-2 p-5" style="background-color: #fffcfa;">
+    <div class="container mt-2 p-5" style="background-color:#fefcfb;;">
       <div class="row">
-        <div class="col-7 me-4" style="background-color: #fffcfa;">
-          <div class="row mb-4 p-3 shadow border-0">
+        <div class="col-7 me-4" style="background-color:#fefcfb;">
+          <div class="row mb-4 p-3" style="background-color: white; border: 1px solid #dbdbdb; border-radius: 10px;">
             <h4>
               <strong>IMPLEMENTATION</strong>
             </h4>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <?php echo $result['implementation']?>
             </p>
           </div>
           
-          <div class="row mb-4 p-3 shadow border-0">
+          <div class="row mb-4 p-3" style="background-color: white; border: 1px solid #dbdbdb; border-radius: 10px;">
             <h4>
               <strong>MTAP COURSE</strong>
             </h4>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <?php echo $result['mtap_course']?>
             </p>
           </div>
 
-        <div class="row mb-4 p-3 shadow border-0">
+        <div class="row mb-4 p-3" style="background-color: white; border: 1px solid #dbdbdb; border-radius: 10px;">
           <h4>
             <strong>PREREQUISITES</strong>
           </h4>
-          <p>
-            Content here.
-          </p>
+          <ul style="padding-left:30px;">
+            <?php 
+                $pre_req = $result["pre_requisite_course"];
+                while($pre_req) {
+                  $sql = "SELECT * FROM course WHERE course_id = '$pre_req'";
+                  $query = mysqli_query($conn, $sql);
+                  $course = mysqli_fetch_assoc($query);
+                ?>
+                  <li>
+                    <a class="fw-bold" href style="color:#9D2426;"><?= $course['course_title'] ?></a>
+                  </li>
+                <?php 
+                  $pre_req = $course['pre_requisite_course'];
+                }
+                ?>
+            <?php ?>
+          </ul>
         </div>
 
           <div class="row mt-5">
@@ -92,8 +109,8 @@
             </div>
           </div>
         </div>
-        <div class="col" style="background-color: #fffcfa;">
-          <div class="row shadow border-0 p-3">
+        <div class="col" style="background-color:#fefcfb;">
+          <div class="row p-3" style="background-color: white; border: 1px solid #dbdbdb; border-radius: 10px;" >
             <h4>
               <strong>INSTRUCTORS</strong>
             </h4>
