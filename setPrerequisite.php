443add5eae4aaa8cca32e1068c90681245a9a1c0
Arc-Data <arclight04@gmail.com>
2023-02-22 10:03:57 +0800

    removed possibility of circular prerequisite dependency

--- setPrerequisite.php
+++ setPrerequisite.php
@@ -1,5 +1,5 @@
 <?php 
-
+	session_start();
 	require 'templates/connection.php';
 	
 	if(!isset($_POST['prerequisite_id'])) {
@@ -9,6 +9,24 @@
 
 	$id = $_GET['id'];
 	$prereq = $_POST['prerequisite_id'];
+
+	# code to check for prerequisite loop
+	$prereq_id = $prereq;
+	while($prereq_id) {
+		$sql = "
+			SELECT * FROM course 
+			WHERE course_id = $prereq_id";
+		$query = mysqli_query($conn, $sql);
+		$course = mysqli_fetch_assoc($query);
+
+		if($id = $course['pre_requisite_course']) {
+			header("location: updateCourse.php?id=$id");
+		}
+
+		$prereq_id = $course['pre_requisite_course'];
+	}
+
+
  
 	$sql = "
 		UPDATE `course` 
