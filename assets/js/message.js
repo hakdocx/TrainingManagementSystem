2da66640bce136f365c173350ee9821c9ad66bfe
Arc-Data <arclight04@gmail.com>
2023-02-22 11:37:48 +0800

    added pop-up class to style.css

---
+++ assets/js/message.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body');
+const popUp = document.createElement('div');
+
+
+const createMessage = () => {
+
+};
+
+popUp.classList.add('pop-up');
+popUp.textContent = "hello world";
+
+body.appendChild(popUp);
