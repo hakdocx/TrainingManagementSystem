38c57f9b745cb6309b2ad6f4eab8ff5ebc421a22
luwesaram <luwesaram@gmail.com>
2023-02-23 11:25:01 +0800

    created if else condition to add course, update course if existing, else proceed to create

--- course/code.php
+++ course/code.php
@@ -5,25 +5,51 @@ require '../templates/connection.php';
 
 if(isset($_POST['save_student']))
 {
+
     $course_title = mysqli_real_escape_string($conn, $_POST['course_title']);
     $number_of_days = mysqli_real_escape_string($conn, $_POST['number_of_days']);
     $mtap_course = mysqli_real_escape_string($conn, $_POST['mtap_course']);
     $implementation = mysqli_real_escape_string($conn, $_POST['implementation']);
 
-    $query = "INSERT INTO course (course_title,number_of_days,mtap_course,implementation) VALUES ('$course_title','$number_of_days','$mtap_course','$implementation')";
-
+    $query = "SELECT course_title FROM course WHERE course_title='$course_title'";
     $query_run = mysqli_query($conn, $query);
-    if($query_run)
-    {
-        $_SESSION['message'] = "Course Created!";
-        header("Location: index.php");
-        exit(0);
-    }
-    else
-    {
-        $_SESSION['message'] = "Course Not Created!";
-        header("Location: index.php");
-        exit(0);
+
+    $result = mysqli_fetch_assoc($query_run);
+    if ($result['course_title']) {
+
+        $query = "UPDATE course SET number_of_days='$number_of_days', mtap_course='$mtap_course', implementation='$implementation' WHERE course_title='$course_title'";
+        $query_run = mysqli_query($conn, $query);
+
+        if($query_run)
+        {
+            $_SESSION['message'] = "Course exists, value updated!";
+            header("Location: index.php");
+            exit(0);
+        }
+        else
+        {
+            $_SESSION['message'] = "Course not updated!";
+            header("Location: index.php");
+            exit(0);
+        }
+
+    } else {
+    
+        $query = "INSERT INTO course (course_title,number_of_days,mtap_course,implementation) VALUES ('$course_title','$number_of_days','$mtap_course','$implementation')";
+        $query_run = mysqli_query($conn, $query);
+
+        if($query_run)
+        {
+            $_SESSION['message'] = "Course Created!";
+            header("Location: index.php");
+            exit(0);
+        }
+        else
+        {
+            $_SESSION['message'] = "Course Not Created!";
+            header("Location: index.php");
+            exit(0);
+        }
     }
 }
 
