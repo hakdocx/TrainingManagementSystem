09a6bf22b322069e29264114d3d6f76456270458
luwesaram <luwesaram@gmail.com>
2023-02-23 15:08:28 +0800

    fixed font sizes

--- course/registrationCourse.php
+++ course/registrationCourse.php
@@ -37,22 +37,22 @@
                 <form class="signUp" action="" method="get">
                   <div class="form-container pe-5 ps-5">
                     <div class="form-row mt-4">
-                      <label class="lbl-name" for='mtap-course'>Instructor ID <label class="asterisk">*</label></label>
-                      <input class="input-box p-2" type="text" name="instructor_id" id ='mtap-course' required>
+                      <label class="lbl-name fs-5" for='mtap-course'>Instructor ID <label class="asterisk">*</label></label>
+                      <input class="input-box p-2 fs-5" type="text" name="instructor_id" id ='mtap-course' required>
                     </div>
                     <div class="form-row-date mt-4">
                       <div class="col me-5">
-                        <label class="lbl-name" for='mtap-course'>Opening Date <label class="asterisk">*</label></label>
-                        <input class="input-box p-2" style='width: 100%;' type="date" name="opening_date" id ='Implementation' required>
+                        <label class="lbl-name fs-5" for='mtap-course'>Opening Date <label class="asterisk">*</label></label>
+                        <input class="input-box p-2 fs-5" style='width: 100%;' type="date" name="opening_date" id ='Implementation' required>
                       </div>
                       <div class="col">
-                        <label class="lbl-name" for='mtap-course'>Closing Date <label class="asterisk">*</label></label>
-                        <input class="input-box p-2" style='width: 100%;' type="date" name="closing_date" id ='Implementation' required>
+                        <label class="lbl-name fs-5" for='mtap-course'>Closing Date <label class="asterisk">*</label></label>
+                        <input class="input-box p-2 fs-5" style='width: 100%;' type="date" name="closing_date" id ='Implementation' required>
                       </div>
                     </div>
                     <div class="form-row mt-4">
-                      <label class="lbl-name" for='mtap-course'>Implementation NR <label class="asterisk">*</label></label>
-                      <input class="input-box p-2" type="text" name="implementation" id ='mtap-course' required>
+                      <label class="lbl-name fs-5" for='mtap-course'>Implementation NR <label class="asterisk">*</label></label>
+                      <input class="input-box p-2 fs-5" type="text" name="implementation" id ='mtap-course' required>
                     </div>
                     <div>
                     <button name="save_student" type="submit" class="btn btn-primary mt-5 fs-5 pe-4 ps-4" id="register-btn" >REGISTER</button> 
