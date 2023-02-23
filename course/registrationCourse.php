60f91f1faa1f29fab507b1a8cffce7b538dec5dd
luwesaram <luwesaram@gmail.com>
2023-02-23 15:11:17 +0800

    added paddings to fit design

--- course/registrationCourse.php
+++ course/registrationCourse.php
@@ -24,7 +24,7 @@
   <body>
     <div class="container">
       <div class="row align-items-center" style="min-height:100%; background-color: white">
-        <div class="col">
+        <div class="col pe-5 ps-5">
           <div class="row">
             <a class="fs-5 pe-5 ps-5" href="" style="text-decoration:none; color:#681A1A;">  
               &#8592; Back to View
