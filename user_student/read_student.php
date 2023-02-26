2d6ae6ff1565f651749eeede7660c50f7d10d861
ron-ligsay <akyligsay16@gmail.com>
2023-02-26 09:31:27 +0800

    mod read_student

--- user_student/read_student.php
+++ user_student/read_student.php
@@ -12,7 +12,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" a href="bootstrap.min.css"/>
+    <link rel="stylesheet" a href="../student/bootstrap.min.css"/>
 	<title></title>
 </head>
 <body>
