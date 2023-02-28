0383060a321b53eb52a79feba393c71bf8e52ce0
Emmanuel0725 <emmanuelfrancismacaya@gmail.com>
2023-02-28 21:19:59 +0800

    Fixing Session

--- assets/popup/message.php
+++ assets/popup/message.php
@@ -7,10 +7,8 @@
 
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
         <div class="col">
-            <i class="gg-check-o"> </i> 
-        </div>  
-        <div class="col">
-            <?= $_SESSION['message']; ?>
+            <strong>&#10003;</strong> <?= $_SESSION['message']; ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>  
     </div>
 
@@ -18,3 +16,37 @@
     unset($_SESSION['message']);
     endif;
 ?>
+
+<style>
+.alert-dismissible {
+    position:fixed;
+	text-align:center;
+	left: 90%;
+	margin-top: 100px;
+}
+
+.alert-warning {
+    color: white;
+    background-color: #00c04b;
+	border-color: #00c04b;
+	border-radius: 5px;
+	width: 10%;
+    height: 7%;
+	padding-right: 3.5%;
+	padding-top: auto;
+	font-size: 15px;
+	text-align: center;
+	box-sizing: border-box;
+	border-left: 3px solid wheat;
+}
+
+.alert-dismissible .btn-close {
+    position: absolute;
+    top: 14%;
+    right: 3.5%;
+    z-index: 2;
+    padding: 1.25rem 1rem;
+    float: left;
+}
+
+</style>
