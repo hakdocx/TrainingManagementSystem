8b40f2af8bbe4115d1dffa0ee21c9f5823fcebf4
Arc-Data <arclight04@gmail.com>
2023-02-28 19:05:40 +0800

    base template for course-registration-details

--- student/course-registration-details.php
+++ student/course-registration-details.php
@@ -1,6 +1,23 @@
 <?php 
 	require '../templates/connection.php'; 
 	require '../templates/header.php';
+
+	// check whether id parameter exists
+	if(!isset($_GET['id'])) {
+		header("location: courses.php");
+	}
+	
+	$registrationId = $_GET['id'];
+
+	// check whether id parameter is invalid
+	$sql = "SELECT * FROM registration_course WHERE registration_course_id = $registrationId";
+	$query = mysqli_connect($con, $sql);
+	$registrationCourse = mysqli_fetch_assoc($query);
+
+	if(!$registrationCourse) {
+		header("location: courses.php");
+	}
+
 ?>
 
 </head>
