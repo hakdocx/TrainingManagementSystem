f0ce5d93ceb3e6f8177673f3129d30c5b2dc5069
luwesaram <luwesaram@gmail.com>
2023-03-02 16:05:24 +0800

    added create student functionality for student module

--- student/create_student.php
+++ student/create_student.php
@@ -1,30 +1,35 @@
 <?php 
 session_start();
-include "connection_student.php";
+include '../templates/connection.php';
 
   if (isset($_POST['submit'])) {
-    
-    $student_id = $_POST['student_id'];
+    $password = $_POST['password'];
+    $username = $_POST['username'];
     $rank= $_POST['rank'];
     $lastname = $_POST['lastname'];
     $firstname = $_POST['firstname'];
     $middlename = $_POST['middlename'];
     $suffix = $_POST['suffix'];
     $office_name = $_POST['office_name'];
-    $sql = "INSERT INTO `students`(`student_id`, `rank` ,`lastname`, `firsttname`,`middlename` ,`suffix`, `office_name`) VALUES ('$student_id','$rank','$lastname','$firstname','$middlename','$suffix','$office_name')";
-    $result = $conn->query($sql);
-   
+    $user_type = 'student';
+    $sql = "INSERT INTO account_details (username,password,user_type,lastname,firstname,middlename,suffix) VALUES ('$username','$password','$user_type','$lastname','$firstname','$middlename','$suffix')";
+    $result = mysqli_query($conn,$sql);
 
+    $sql="SELECT max(account_id) AS acc_id FROM account_details";
+    $result = mysqli_query($conn,$sql);
+    $account_id = mysqli_fetch_assoc($result);
+    $acc_id = $account_id['acc_id'];
+    
+    $sql = "INSERT INTO student(rank, office_name,account_id) VALUES ('$rank','$office_name','$acc_id')";
+    $result = mysqli_query($conn,$sql);
+    
     if ($result == TRUE) {
       echo "New record created successfully.";
     }else{
       echo "Error:". $sql . "<br>". $conn->connect_error;
     } 
-
     $conn->close(); 
-
   }
-
 ?>
 
 <!DOCTYPE html>
@@ -37,38 +42,71 @@ include "connection_student.php";
 	<link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-	
-
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-	<link rel="stylesheet" type="text/css" href="./style.css">
+  <style>
+    input,select {
+      border: 2px solid #DBDBDB;
+      border-radius: 10px;
+      width: 200px;
+    }
+  </style>
 </head>
 <body style="text-align:center; font-family: Montserrat;">
   <?php
-    require '../templates/navigation.php'
+    require '../templates/navigation.php';
   ?>
-<h2 class="mt-4">Student Information </h2>
+<div class="container mt-5">
+  <h2 class="pt-4">Add Student</h2>
+  <form action="" method="POST">
+    <fieldset>
+      <label for="email">Email</label>
+      <br>
+      <input class="p-1 fs-6 mb-2" type="text" name="username" id="email">
+      <br>
+      <label for="password">Password</label>
+      <br>
+      <input class="p-1 fs-6 mb-2" type="text" name="password" id="password">    
+      <br>      
+      <label for="first-name">First Name</label>
+      <br>
+      <input class="p-1 fs-6 mb-2" type="text" name="firstname" id="first-name">
+      <br>
+      <label for="middle-name">Middle Name</label>
+      <br>
+      <input class="p-1 fs-6 mb-2" type="text" name="middlename" id="middle-name">
+      <br>
+      <label for="last-name">Last Name</label>
+      <br>
+      <input class="p-1 fs-6 mb-2" type="text" name="lastname" id="last-name">
+      <br>
+      <label for="suffix">Suffix</label>
+      <br>
+      <select class="p-1 fs-6 mb-2" type="text" name="suffix" id="suffix">
+        <option value=""></option>
+        <option value="Jr.">Jr.</option>
+      </select>
+      <br>
+      <label for="rank">Rank</label>
+      <br>
+      <select class="p-1 fs-6 mb-2" type="text" name="rank" id="rank">
+        <option value="PCO">PCO</option>
+        <option value="PNCO">PCNO</option>
+        <option value="NUP">NUP</option>
+      </select>
+      <br>
+      <label for="office-name">Office Name</label>
+      <br>
+      <select class="p-1 fs-6 mb-4" type="text" name="office_name" id="office-name">
+        <option value="InfoTech">InfoTech</option>
+        <option value="AVG">AVG</option>
+        <option value="TechnoOrg">TechnoOrg</option>
+      </select>
+      <br>
+      <button class="btn btn-primary pe-4 ps-4 pt-2 pb-2" type="Submit" name="submit" value="submit" style="background-color: #681A1A; border:2px solid #681A1A">ADD</button>
+    </fieldset>
+  </form>
 
-<form action="" method="POST">
-  <fieldset>
-    <legend>Personal information:</legend>
-    First name:<br>
-    <input type="text" name="firstname">
-    <br>
-    Last name:<br>
-    <input type="text" name="lastname">
-    <br>
-    Student ID:<br>
-    <input type="text" name="student_id">
-    <br>
-    Course & Section:<br>
-    <input type="text" name="course_sec">
-    <br>
-    <br>
-    <input type="Submit" name="Submit" value="Submit">
-  </fieldset>
-
-</form>
+</div>  
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 </body>
