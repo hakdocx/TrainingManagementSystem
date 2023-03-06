50415376fa6fdfa59e84b4957f04e55ddd1cd008
ron-ligsay <akyligsay16@gmail.com>
2023-03-06 09:20:58 +0800

    update user index

--- user/index.php
+++ user/index.php
@@ -1,67 +1,143 @@
 <?php
-    #require dirname(__DIR__).("/templates/connection_2.php");
-    //session_start();
-    require '../templates/connection.php';
+
+ob_start();
+require dirname(__DIR__).('../templates/connection.php');
+    if(isset($_POST['btnLogout']))
+{
+    session_destroy();
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login System</title>
+    <title>Training Management System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/Navigation-Style.css"/>
+    <link rel="stylesheet" href="../assets/css/index.css">
 
-    <style>
-        body {
-            font-family: 'Montserrat', sans-serif;
-        }
-    </style>
 </head>
+
 <body>
-    <br><br><br>
     <header>
         <?php
             session_start();
-            #require dirname(__DIR__).('navigation.php');
-            //require '../navigation.php';
             require '../templates/navigation.php';
         ?>
     </header>
+        <section>
+            <div class="panel">
+                <div class="course-type">
+                    <img src="../assets/images/icon1.png" alt="" class="course-img">
+                    <p class="course-heading">Upcoming</p>
+                    <table class="tbl">
+                        <thead>
+                            <tr>
+                                <td class="headers">Course Title</td>
+                                <td class="headers">Opening Date</td>
+                                <td class="headers">Closing Date</td>
+                            </tr>
+                    <?php
 
-    <div align="center">
-    <br margin:30 >    <?php
-        if(isset($_GET['success'])){
-            if($_GET['success'] == 'loggedOut'){
-                ?>
-                    <small class='alert alert-info'>You've been logged out successfully!</small>
+                    $sql = "SELECT * 
+                            FROM `course` AS C, `registration_course` AS R 
+                            WHERE C.course_id = R.course_id";
+                    $result = mysqli_query($conn, $sql);
+                    if ($result) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $course_title = $row['course_title'];
+                            $opening_date = $row['opening_date'];
+                            $closing_date = $row['closing_date'];
                     
-                <?php
-            }else if($_GET['success'] == 'loggedIn'){
-                ?>
-                    <small class='alert alert-success'>You've logged in successfully!</small>
-                <?php
-            }
-        }
+                    echo ' 
+                    <tr>
+                        <td>' . $course_title . '</td>
+                        <td>' . $opening_date . '</td>
+                        <td>' . $closing_date . '</td>
+                    </tr>';
+                        }
+                    }
+                    ?>
+                    </table>
+                </div>
+                
 
-    ?>
-        <br>
-        <hr class="line">
-        <br>
-       <?php
+                <div class="course-type">
+                    <img src="../assets/images/icon2.png" alt="" class="course-img">
+                    <p class="course-heading">Ongoing</p>
+                    <table class="tbl">
+                        <thead>
+                            <tr>
+                                <td class="headers">Course Title</td>
+                                <td class="headers">Opening Date</td>
+                                <td class="headers">Closing Date</td>
+                            </tr>
+                    <?php
 
-        if(!isset($_SESSION['username'])){
-            ?>
-                <h2>Kindly login or register an account to start your session.</h2>
-            <?php
-        }else{
-            ?>
-                <h3>Hello, <?php echo ucfirst($_SESSION['username']); ?> !</h3>
+                    $sql = "SELECT * 
+                            FROM `course` AS C, `registration_course` AS R 
+                            WHERE C.course_id = R.course_id";
+                    $result = mysqli_query($conn, $sql);
+                    if ($result) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $course_title = $row['course_title'];
+                            $opening_date = $row['opening_date'];
+                            $closing_date = $row['closing_date'];
+                    
+                    echo ' 
+                    <tr>
+                        <td>' . $course_title . '</td>
+                        <td>' . $opening_date . '</td>
+                        <td>' . $closing_date . '</td>
+                    </tr>';
+                        }
+                    }
+                    ?>
+                    </table>
+                </div>
 
-            <?php
-        }
-       ?>
-    </div>
+               <div class="course-type">
+                    <img src="../assets/images/icon3.png" alt="" class="course-img">
+                    <p class="course-heading">Finished</p>
+                    <table class="tbl">
+                        <thead>
+                            <tr>
+                                <td class="headers">Course Title</td>
+                                <td class="headers">Opening Date</td>
+                                <td class="headers">Closing Date</td>
+                            </tr>
+                    <?php
+
+                    $sql = "SELECT * 
+                            FROM `course` AS C, `registration_course` AS R 
+                            WHERE C.course_id = R.course_id";
+                    $result = mysqli_query($conn, $sql);
+                    if ($result) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $course_title = $row['course_title'];
+                            $opening_date = $row['opening_date'];
+                            $closing_date = $row['closing_date'];
+                    
+                    echo ' 
+                    <tr>
+                        <td>' . $course_title . '</td>
+                        <td>' . $opening_date . '</td>
+                        <td>' . $closing_date . '</td>
+                    </tr>';
+                        }
+                    }
+                    ?>
+                    </table>
+                </div>
+
+            </div>
+        </section>
 </body>
+
 </html>
